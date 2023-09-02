a9b03b6948e15769dbf66d3e6560442a7e779014
MMKT1977 <kennedy.moranga@student.moringaschool.com>
2023-09-03 01:59:13 +0300

    Arrays and State

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,23 +3,46 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
+  
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    
   }
-
+  function handleLiClick(id){
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+    
+  }
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  const [filterBy, setFilterBy]= useState("All");
+
+function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+    
+  }
+  
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
-    </div>
+         <ul>{foodList}</ul>
+
+  <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
+  </div>
+  
   );
 }
 
